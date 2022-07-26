3a1e4d49b49c126189d3a48e3b0f07d6e0749ef1
2dami <ldami8955@gmail.com>
2022-07-26 17:14:59 +0900

    변수사용 중국집 계산(수정)

--- day02/src/data/Test03.java
+++ day02/src/data/Test03.java
@@ -4,7 +4,7 @@ public class Test03 {
       public static void main(String[] args) {
     	  //변수(variable)를 사용해서 Test02를 개선
     	  
-    	  //변수 준비
+    	  //변수 준비 (준비단계)
     	  int jjajang = 5000;//jjajang이란 이름의 저장소에 5000 저장!
     	  int champon = 6000;
     	  int jjajangCount = 2;
@@ -13,11 +13,13 @@ public class Test03 {
     	  //System.out.println(jjajang * jjajangCount);
     	  //System.out.println(champon * champonCount);
     	  //System.out.println(jjajang * jjajangCount + champon * champonCount);
-    	  
+    	 
+    	  //계산단계
     	  int jjajangTotal = jjajang * jjajangCount;//계산결과를 저장!
     	  int champonTotal = champon * champonCount;
     	  int total = jjajangTotal + champonTotal;
     			  
+    	  //출력단계
     			  System.out.println(jjajangTotal);
     	  System.out.println(champonTotal);
     	  System.out.println(total);
