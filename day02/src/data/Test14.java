212243a2e62f56ea620daeef276b7659b0bd5d8f
2dami <ldami8955@gmail.com>
2022-07-27 10:08:47 +0900

    BMI 지수 계산(숙제-수정)

--- day02/src/data/Test14.java
+++ day02/src/data/Test14.java
@@ -3,6 +3,7 @@ package data;
 //구하는 공식은 'BMI = 체중(kg) / 키(cm)' 입니다.
 //키가 180cm이고, 체중이 80kg인 사람의 BMI 지수를 출력
 import java.lang.*;
+import java.text.DecimalFormat;
 public class Test14 {
 	public static void main(String[] args) {
 		
@@ -19,5 +20,14 @@ public class Test14 {
 		System.out.println("키가 180cm이고, 체중이 80kg인 사람의 BMI 지수");
 		System.out.println(bmi);
 		// [수정한이유] 신장을 실수로 형 변환 할때, 변환한 변수명을 그냥 a로 만듦 (heightM로 변경)
+		
+		//소수점 2자리수까지만 나타내라.
+		//(1) 직접구현
+		double bmi2 = (int)(bmi * 100) / 100.0;
+		System.out.println(bmi2);
+		
+		// (2) 라이브러리 사용
+		DecimalFormat fm = new DecimalFormat("0.00");
+		System.out.println(fm.format(bmi));
 }
 }
