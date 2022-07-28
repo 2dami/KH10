1a5581e3789659a549f56bd7c00a9106518f1023
2dami <ldami8955@gmail.com>
2022-07-28 14:17:06 +0900

    I/O(입출력)

---
+++ day04/src/io/Test01.java
@@ -0,0 +1,24 @@
+package io;
+import java.lang.*;
+public class Test01 {
+public static void main(String[] args) {
+		// I/O -Input / Output , 입출력 (기준은 CPU)
+	// 표준 출력 - sytem.out 을 이용하여 글자를 내보내는 행위
+	//표준 입력 - system.in 을 이용하여 글자를 받아들이는 행위
+	//- 저장의 경우, CPU에서 HDD등에 내보내는 경로이므로 output
+	
+	//System.out.printl(i)n(e)()은 화면에 글자를 내보내고 줄을 바꾸는 명령
+	//System.out.print()는 화면에 글자를 내보내는 명령
+	//System.out.printf()는 C언어 개발자를 위해 만든 출력 명령
+	
+	System.out.println("Hello");
+	System.out.println("Hello");
+
+	System.out.print("Hello");
+	System.out.print("Hello");
+	System.out.print("\n");
+
+	//System.out.printf("%d + %d = %d\n", 10,20,30);  ==>c언어 개발자용
+}
+	}
+
