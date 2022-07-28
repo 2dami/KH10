5884f411fdabb5ad18544cfe3c97a78515815182
2dami <ldami8955@gmail.com>
2022-07-28 14:21:52 +0900

    표준입력 (System.in) 사용 문제점

---
+++ day04/src/io/Test02.java
@@ -0,0 +1,19 @@
+package io;
+//System.in만을 사용하여 입력을 받는 예제
+//import java.io.IOException;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) { // throws IOException 
+	
+		//int a = System.in.read();
+	//	System.out.println("a=" + a);
+	}
+}
+
+
+//System.in만을 사용하여 입력받을때 문제점
+
+		//1.하나의 명령이 한 글자만 입력받는다.
+		//2.입력을 아스키코드표 숫자로 변환해서 받는다.
+		//3.예외 처리를 해야한다.
+		//4.자료형을 선택할 수 없다.
