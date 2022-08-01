54483c84c9c3cf97e7d5422ac36a11348f6aac98
2dami <ldami8955@gmail.com>
2022-08-01 16:50:11 +0900

    random (구구단문제)

---
+++ day06/src/random/Test03.java
@@ -0,0 +1,27 @@
+package random;
+import java.util.Random;
+import java.util.Scanner;
+
+//랜덤으로 구구단 문제를 하나 출제하고 사용자가 정답을 입력하면 정답과 오답을 판정하는 프로그램을 구현
+//단, 문제는 2x1부터 9x9의 범위 내에서 출제해야 합니다
+public class Test03 {
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		int a = r.nextInt(8)+2;
+		
+		Random r2 = new Random();
+		int b = r.nextInt(9)+1;
+		
+		System.out.println(a + "x" + b );
+		
+		Scanner sc = new Scanner(System.in);
+		int user = sc.nextInt();
+		if ( a * b == user) {
+			System.out.println(" 정답 " );
+		}
+		else {
+			System.out.println(" 오답 " );
+		}
+	}
+}
