aee16d0f5aa3702b0b04a6c8b3ef809f164426ca
2dami <ldami8955@gmail.com>
2022-08-02 15:38:52 +0900

    점심메뉴 추첨

---
+++ day07/src/array/Test10.java
@@ -0,0 +1,10 @@
+package array;
+// [점심 메뉴 추첨 프로그램]
+// 사용자에게 입력할 메뉴의 개수를 입력받아 해당 크기만큼의 배열을 생성
+// 생성된 배열에 점심메뉴를 입력
+// 입력이 완료되면 입력된 메뉴 중에서 랜덤으로 한 개의 메뉴를 추첨한 뒤 출력
+public class Test10 {
+public static void main(String[] args) {
+	
+}
+}
