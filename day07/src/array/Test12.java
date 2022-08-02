87ac1f1b3390b36833b0e49d858ccbe62de8c95d
2dami <ldami8955@gmail.com>
2022-08-02 18:21:49 +0900

    최소값 찾아 배열바꾸기

--- day07/src/array/Test12.java
+++ day07/src/array/Test12.java
@@ -8,5 +8,25 @@ package array;
 // 사이트 참고 [https://visualgo.net/en/sorting] (https://visualgo.net/en/sorting)
 public class Test12 {
 public static void main(String[] args) {
+	
+	int[] data = new int [] {30, 20, 10, 50, 40};
+	
+	//1턴 : 전체 배열에서 가장 작은 숫자의 위치를 찾아 맨 앞 데이터와 교체
+	
+	//최소값 찾기
+	int min = 0;
+	for (int i=1; i <= 4; i++) {
+		if(data[min] > data[i]) { //더 작은값을 발견하면
+			min =i;
+		}
+	}
+	System.out.println("min =" + min);
+	
+	//교체
+	int backup = data[0];
+	data[0] = data[min];
+	data[min] = backup;
+	
+
 }
 }
