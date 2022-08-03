f257203354b5ebbce91acfd9cf0bbc5a22a478d5
2dami <ldami8955@gmail.com>
2022-08-03 18:21:36 +0900

    07

--- day08/src/array2d/Test07.java
+++ day08/src/array2d/Test07.java
@@ -1,4 +1,7 @@
 package array2d;
+
+import java.util.Random;
+
 // 다음 규칙에 따라 지뢰찾기를 만드세요
 // - 9x9 크기의 정수 배열을 준비
 // - 지뢰는 9로 표시하며, 배열 내에 무작위로 20개가 배치된다.
@@ -7,5 +10,19 @@ package array2d;
 public class Test07 {
 public static void main(String[] args) {
 	
+	//준비
+	int size = 9;
+	
+	int[][] map = new int [size][size];
+	
+
+	Random r = new Random();
+	for(int i=0; i< size ; i++) {
+		for(int k=0; k< size; ) {
+	}
+	
+	
+	
+}
 }
 }
