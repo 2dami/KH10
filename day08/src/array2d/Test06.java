8ad31127935fbcb39e3a827f65ea70975f24b047
2dami <ldami8955@gmail.com>
2022-08-03 18:07:32 +0900

    빙고판 만들기

--- day08/src/array2d/Test06.java
+++ day08/src/array2d/Test06.java
@@ -1,4 +1,6 @@
 package array2d;
+import java.util.Random;
+
 //다음 규칙에 따라 빙고판을 만드세요
 //- 5x5 크기의 정수 배열을 준비
 //- 1부터 25까지의 숫자를 무작위 위치에 배치
@@ -6,6 +8,28 @@ package array2d;
 //- 크기가 달라질 경우에도 달라진 크기에 맞게 빙고판이 만들어져야 합니다
 public class Test06 {
 public static void main(String[] args) {
+	//준비
+	int size = 5;
+	
+	int[][] map = new int[size][size];
+	//map.length 와 map[?].length가 모두 size와 같다.
+	
+	//계산
+	Random r = new Random();
+	for(int i=0; i < size; i++) {
+		for(int k=0; k < size; k++) {
+			map[i][k] = r.nextInt(25) + 1;
+		}
+	}
+	//출력
+	for(int i=0; i < size; i++) {
+		for(int k=0; k < size; k++) {
+			System.out.print(map[i][k]);
+			System.out.print("\t");
+		}
+		System.out.println();
+		System.out.println();
+	}
 	
 }
 }
