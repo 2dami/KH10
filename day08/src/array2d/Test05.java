1d86f428f3f0c5e581f3f786d322b664256af0fe
2dami <ldami8955@gmail.com>
2022-08-03 14:48:05 +0900

    2차원 문제

---
+++ day08/src/array2d/Test05.java
@@ -0,0 +1,8 @@
+package array2d;
+//Test03번과 동일하게 줄과 칸 수를 입력받아 2차원 배열을 생성하고 초기화는 대각선 방향으로 하세요
+//줄 수 : 3 / 칸 수 : 4
+public class Test05 {
+public static void main(String[] args) {
+	
+}
+}
---
+++ day08/src/array2d/Test07.java
@@ -0,0 +1,11 @@
+package array2d;
+// 다음 규칙에 따라 지뢰찾기를 만드세요
+// - 9x9 크기의 정수 배열을 준비
+// - 지뢰는 9로 표시하며, 배열 내에 무작위로 20개가 배치된다.
+// - 배치된 이후 전체 지도를 출력하세요
+
+public class Test07 {
+public static void main(String[] args) {
+	
+}
+}
