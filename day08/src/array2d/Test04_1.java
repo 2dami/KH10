067616e73a8bdd106f9d2bc62bd3019d49d9a76d
2dami <ldami8955@gmail.com>
2022-08-03 16:57:12 +0900

    Test04 풀이

--- day08/src/array2d/Test04_1.java
+++ day08/src/array2d/Test04_1.java
@@ -4,30 +4,40 @@ import java.util.Scanner;
 
 //Test03번과 동일하게 줄과 칸 수를 입력받아 2차원 배열을 생성하고 초기화는 세로 방향으로 하세요
 // 줄 수 : 3 / 칸 수 : 4
-public class Test04 {
+public class Test04_1 {
 public static void main(String[] args) {
 	
 	//준비
 	Scanner sc = new Scanner(System.in);
-	System.out.print("칸 수: ");
+	System.out.print("줄 수: ");
 			int row = sc.nextInt();
-			System.out.print("줄 수: ");
+			System.out.print("칸 수: ");
 			int col = sc.nextInt();
+			sc.close();
 			
 			int[][] arr = new int[row][col];
 			//arr.length는 row와 같다
 			//arr[?].length는 col과 같다
 			
 			//계산
-			//-(줄,칸) 기준
-			//(0,0)-->(1,0)-->(2,0)-->(0,1)-->(1,1)-->(2,1)-->...
-			int n = 1;
-			for(int i = 0; i <= col; i++) {
-				for(int k =0; k < row; k++) {
-				arr[k][i] =n;
-					n++;
+			//- 값 기준
+			//1→2→3→...→12(row*col)
+			int x = 0;
+			int y = 0;
+			for(int i=1; i <= row*col; i++) {
+				//System.out.println(i+" → ("+x+", "+y+")");
+				arr[x][y] = i;
+				
+				if(x < row-1) {//마지막 줄이 아니라면
+					x++;//다음줄
+				}
+				else {//마지막 줄이라면
+					//다음 칸 첫번째 줄로 이동
+					y++;
+					x = 0;
 				}
 			}
+			
 			//출력
 			for(int i=0; i < arr.length; i++) {
 				for(int k=0; k < arr[i].length; k++) {
@@ -37,6 +47,4 @@ public static void main(String[] args) {
 				System.out.println();
 			}
 		}
-}
-
-
+	}
