962aa9b79bdd774038d037cb2243f01e3f9cc4b0
2dami <ldami8955@gmail.com>
2022-08-03 10:26:43 +0900

    선택정렬 부분풀이

--- day07/src/array/Test12.java
+++ day07/src/array/Test12.java
@@ -9,7 +9,7 @@ package array;
 public class Test12 {
 public static void main(String[] args) {
 	
-	int[] data = new int [] {30, 20, 10, 50, 40};
+	int[] data = new int [] {30, 50, 20, 10, 40};
 	
 	//1턴 : 전체 배열에서 가장 작은 숫자의 위치를 찾아 맨 앞 데이터와 교체
 	
@@ -26,7 +26,10 @@ public static void main(String[] args) {
 	int backup = data[0];
 	data[0] = data[min];
 	data[min] = backup;
-	
 
+	//출력
+	for(int i=0; i < data.length; i++) {
+		System.out.println(data[i]);
+	}
 }
 }
