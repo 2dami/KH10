068b6ec74e822d8440c982c3722afd17f7175f5e
2dami <ldami8955@gmail.com>
2022-08-03 09:56:38 +0900

    선택정렬 부분풀이

--- day07/src/array/Test12_1.java
+++ day07/src/array/Test12_1.java
@@ -3,13 +3,13 @@ public class Test12_1 {
 public static void main(String[] args) {
 	int[] data = new int[] {30, 50, 20, 10, 40};
 	
+	for(int i=0; i <data.length-1; i++) {
 	//1턴 : 전체 배열에서 가장 작은 숫자의 위치를 찾아 맨 앞에 데이터와 교체
-	
 	//최소값 찾기
-	int min = 2;
-	for(int i=min+1; i <= 4; i++) {
-		if(data[min] > data[i]) {//더 작은 값을 발견하면
-			min = i;
+	int min = i;
+	for(int k=min+1; k < data.length; k++) {
+		if(data[min] > data[k]) {//더 작은 값을 발견하면
+			min = k;
 		}
 	}
 	System.out.println("min = " + min);
@@ -18,10 +18,11 @@ public static void main(String[] args) {
 	int backup = data[2];
 	data[2] = data[min];
 	data[min] = backup;
-	
+	}
 	//출력
 	for(int i=0; i < data.length; i++) {
 		System.out.println(data[i]);
 	}
 }
 }
+
