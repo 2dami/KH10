4cbe038e17c4a6fed5234b831f7790fff9aba517
2dami <ldami8955@gmail.com>
2022-08-04 14:36:31 +0900

    메소드의 활용

--- day09/src/oop/mathod1/Student.java
+++ day09/src/oop/mathod1/Student.java
@@ -14,8 +14,8 @@ public class Student {
 	// -실행하려면 string과 int를 하나씩 전달해야 하는 메소드
 	// -외부에서 전달된 값을 저장하느 변수를 "매개변수"라고 합니다
 	void setting (String name, int score) { //void setting(이름,점수) {
-		this.name = "피카츄";
-		this.score = 90;
+		this.name = name;
+		this.score = score;
 	}
 	//출력 메소드 (코드 저장소)
 	void print() {
