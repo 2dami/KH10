b498241eef504cc35471b416e580bb7191370078
2dami <ldami8955@gmail.com>
2022-08-04 12:25:40 +0900

    올림픽 메달선수 (객체지향)

--- day09/src/oop/mathod2/Test02.java
+++ day09/src/oop/mathod2/Test02.java
@@ -7,9 +7,13 @@ public class Test02 {
 	a.setting("진종오" , "사격" , "하계" , 4 , 2 , 0);
 	a.print();
 	
+	System.out.println("\t");
+	
 	Player b = new Player();
 	b.setting("김수녕" , "양궁" , "하계" , 4 , 1 , 1);
 	b.print();
+	
+	System.out.println("\t");
 	
 	Player c = new Player();
 	c.setting("전이경" , "쇼트트랙" , "동계" , 4 , 0 , 1);
