f9422b09cba22fcfcf80bb4d28ea8d7cb64cffcf
2dami <ldami8955@gmail.com>
2022-08-04 18:12:37 +0900

    카페 문제풀이

--- day09/src/odd/method4/Menu.java
+++ day09/src/odd/method4/Menu.java
@@ -4,10 +4,16 @@ public class Menu {
 	String name;
 	String part;
 	int price;
-	String event;
+	//String event;
+	boolean event;
 	
-void setting (String name,String part,int price,String event) {
+	//method
 	
+	//method overloading : 같은 이름으로 메소드를 여러개 준비하는 것 
+void setting (String name,String part,int price) {
+	this.setting(name,part,price,false);
+}
+void setting (String name,String part,int price,boolean event) {
 	this.name= name;
 	this.part = part;
 	this.price =price;
@@ -17,7 +23,10 @@ void print() {
 	System.out.println( " <커피숍 메뉴정보> ");
 System.out.println( "이름 : " + this.name);
 System.out.println( "분류 : " + this.part);
-System.out.println( "가격 : " + this.price);
-System.out.println( "행사여부 : " + this.event);
+System.out.println( "가격 : " + this.price+"원");
+if(this.event == true) {
+	System.out.print("행사상품");
+}
+System.out.println( );
 }
 }
--- day09/src/odd/method4/Test04.java
+++ day09/src/odd/method4/Test04.java
@@ -14,10 +14,10 @@ public class Test04 {
 	Menu c = new Menu();
 	Menu d = new Menu();
 	
-	a.setting("아메리카노","음료",2500, "행사중");
-	b.setting("모카라떼","음료",3500, " - ");
-	c.setting("치즈케이크","디저트",5000, "행사중" );
-	d.setting("마카롱","디저트",3000, " - " );
+	a.setting("아메리카노","음료",2500, true);
+	b.setting("모카라떼","음료",3500 );
+	c.setting("치즈케이크","디저트",5000, true);
+	d.setting("마카롱","디저트",3000 );
 	
 	a.print();
 	b.print();
