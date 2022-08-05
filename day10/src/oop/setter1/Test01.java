df3165796cabf5bd70cc816bd0fc6a8b7abaaebd
2dami <ldami8955@gmail.com>
2022-08-05 13:26:44 +0900

    setter method 예제

--- day10/src/oop/setter1/Test01.java
+++ day10/src/oop/setter1/Test01.java
@@ -3,7 +3,7 @@ package oop.setter1;
 public class Test01 {
 	public static void main(String[] args) {
 
-		Car a = new Car();
+		Car a = new Car("아반떼", 200, 5, 15000000);
 		
 //		(문제점) 변수는 이상데이터 방지가 불가능
 //		a.name = "아반떼";
