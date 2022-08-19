8d888afe44d9118a83a2fe104817ec0f2eaaed57
2dami <ldami8955@gmail.com>
2022-08-19 10:51:06 +0900

    좋아요 설정/해제

--- day17/src/api/util/collection2/Test03.java
+++ day17/src/api/util/collection2/Test03.java
@@ -1,5 +1,6 @@
 package api.util.collection2;
 
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,11 +13,42 @@ import java.util.TreeSet;
 public class Test03 {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		Set<Integer> list =  new TreeSet<>();
+		Set<Integer> like =  new TreeSet<>();
 		
+		Scanner sc = new Scanner(System.in);
+		while(true) {
 		System.out.println("글 번호 :");
-	
+		int number = sc.nextInt();
+		if(number == 0) { //0을 입력하면 종료되도록 처리(가장 먼저 검사)
+			break;
+		}
+		
+		
+		if (like.contains(number)) {
+			like.remove(number);
+			System.out.println(number + "번호 게시글에 좋아요를 취소했습니다.");
+		}
+		else {
+		like.add(number);
+		System.out.println(number + "번호 게시글에 좋아요를 눌렀습니다.");
 	}
-
+}
+		sc.close();
+		//목록 출력
+		System.out.println(like);
+		
+		//개별 접근은 불가능하지만 전체를 다 추출하는것은 가능하다.
+		//1. Iterator이라는 별개의 저장소로 이동.
+		Iterator<Integer> iter = like.iterator();
+		while(iter.hasNext()) {
+			int number = iter.next();
+			System.out.println(number);
+		}
+		//2. 확장형 for 구문을 사용
+		for(int number : like) { // like에 있는 데이터 개수만큼 몽땅
+			System.out.println(number);
+		}
+		//3. 배열로 복사하거나 List로 복사.(비추천)
+		
+	}
 }
