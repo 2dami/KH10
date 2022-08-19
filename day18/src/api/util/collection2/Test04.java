b0410a390dd3bbe53b1bb68a1e86bca0c9b8f1f6
2dami <109942440+2dami@users.noreply.github.com>
2022-08-19 17:32:50 +0900

    Update Test04.java

--- day18/src/api/util/collection2/Test04.java
+++ day18/src/api/util/collection2/Test04.java
@@ -16,8 +16,10 @@ public class Test04 {
 		a.add("라이츄");    b.add("루이지");
 		a.add("뮤츠");       b.add("뮤츠");
 		
-		System.out.println("a =" + a);
-		System.out.println("b =" + b);
+		//글자에도 (해시)코드가 있기 때문에 정렬이 가능하다
+		
+		System.out.println("a =" + a); //정렬된거처럼 보이지 않음
+		System.out.println("b =" + b); //Tree는 정렬이 되어서 보인다
 	}
 
 }
